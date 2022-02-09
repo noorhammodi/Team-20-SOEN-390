735e45dc8ad718d078c832c591b6ae7b434926cc
Laurent Lao <lao.laurent@gmail.com>
2022-02-08 20:35:57 -0500

    Fix most linter issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
 import { StyleSheet, Text, View } from 'react-native';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomePage from './views/HomePage';
-import { LoginScreen } from './views/LoginScreen';
+import LoginScreen from './views/LoginScreen';
 import Dashboard from './views/Dashboard';
-
-function App() {
-  return (
-    <View style={styles.container}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/login" element={<LoginScreen/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-        </Routes>
-      </Router>
-
-
-
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -33,5 +17,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+      {/* eslint-disable-next-line react/style-prop-object */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 export default App;
--- frontend/src/views/__test__/LoginScreen.test.js
+++ frontend/src/views/__test__/LoginScreen.test.js
@@ -1,14 +1,17 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import { LoginScreen } from "../LoginScreen";
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import { toBeInTheDocument } from '@testing-library/jest-dom'
+// eslint-disable-next-line no-unused-vars
+import { toBeInTheDocument } from '@testing-library/jest-dom';
+import LoginScreen from '../LoginScreen';
 
-const MockLogin = () => {
-    return (
-        <BrowserRouter>
-            <LoginScreen />
-        </BrowserRouter>
-    )
+function MockLogin() {
+  return (
+    <BrowserRouter>
+      <LoginScreen />
+    </BrowserRouter>
+  );
 }
 
 it("should have initial title 'Signup'", async () => {
@@ -18,9 +21,9 @@ it("should have initial title 'Signup'", async () => {
 });
 
 it("should have changed title 'Login'", async () => {
-    render(<MockLogin />);
-    const submitButton = screen.getByText(/sign up|log in/i)
-    fireEvent.click(submitButton)
-    const title = screen.getByText(/login/i);
-    expect(title).toBeInTheDocument();
-  });
+  render(<MockLogin />);
+  const submitButton = screen.getByText(/sign up|log in/i);
+  fireEvent.click(submitButton);
+  const title = screen.getByText(/login/i);
+  expect(title).toBeInTheDocument();
+});

a7d3cc9bbdbc1624ff19592fa2b3b022da7dc094
denizakcal <denizakcal@mail.com>
2022-02-24 14:12:03 -0500

    #32 Add Temperature attribute to health form according to feverOrChills (#114)
    Co-authored-by: Laurent Lao <32573725+laurent-lao@users.noreply.github.com>

--- controllers/formsRouter.js
+++ controllers/formsRouter.js
@@ -10,25 +10,52 @@ formsRouter.post('/healthform', async (request, response) => {
   // Get request.body and put it in new var body
   const { body } = request;
 
-  // Using body, set new payload
-  const healthform = new HealthForm({
-    feverOrChills: body.feverOrChills,
-    suddenLossOfSenseOfSmellAndTaste: body.suddenLossOfSenseOfSmellAndTaste,
-    difficultyBreathingOrShortnessOfBreath: body.difficultyBreathingOrShortnessOfBreath,
-    cough: body.cough,
-    runnyOrStuffyNose: body.runnyOrStuffyNose,
-    outsideCanadaTravellingInPast14Days: body.outsideCanadaTravellingInPast14Days,
-    closeContactWithSuspectedCase: body.closeContactWithSuspectedCase,
-    unusualSevereFatigue: body.unusualSevereFatigue,
-    unusualHeadache: body.unusualHeadache,
-    significantLossOfAppetite: body.significantLossOfAppetite,
-    unusualOrUnexplainedMusclePainOrStiffness: body.unusualOrUnexplainedMusclePainOrStiffness,
-    soreThroatWithoutObviousCause: body.soreThroatWithoutObviousCause,
-  });
+  if (Object.keys(body).length === 12 && body.feverOrChills === false) {
+    // Using body, set new payload
+    const healthform = new HealthForm({
+      feverOrChills: body.feverOrChills,
+      suddenLossOfSenseOfSmellAndTaste: body.suddenLossOfSenseOfSmellAndTaste,
+      difficultyBreathingOrShortnessOfBreath: body.difficultyBreathingOrShortnessOfBreath,
+      cough: body.cough,
+      runnyOrStuffyNose: body.runnyOrStuffyNose,
+      outsideCanadaTravellingInPast14Days: body.outsideCanadaTravellingInPast14Days,
+      closeContactWithSuspectedCase: body.closeContactWithSuspectedCase,
+      unusualSevereFatigue: body.unusualSevereFatigue,
+      unusualHeadache: body.unusualHeadache,
+      significantLossOfAppetite: body.significantLossOfAppetite,
+      unusualOrUnexplainedMusclePainOrStiffness: body.unusualOrUnexplainedMusclePainOrStiffness,
+      soreThroatWithoutObviousCause: body.soreThroatWithoutObviousCause,
+    });
 
-  // Send the payload via mongoose, wait for response then return it
-  const savedHealthForm = await healthform.save();
-  response.json(savedHealthForm);
+    // Send the payload via mongoose, wait for response then return it
+    const savedHealthForm = await healthform.save();
+    response.json(savedHealthForm);
+  } else if (Object.keys(body).length === 13 && body.feverOrChills === true) {
+    // Using body, set new payload
+    const healthform = new HealthForm({
+      feverOrChills: body.feverOrChills,
+      temperature: body.temperature,
+      suddenLossOfSenseOfSmellAndTaste: body.suddenLossOfSenseOfSmellAndTaste,
+      difficultyBreathingOrShortnessOfBreath: body.difficultyBreathingOrShortnessOfBreath,
+      cough: body.cough,
+      runnyOrStuffyNose: body.runnyOrStuffyNose,
+      outsideCanadaTravellingInPast14Days: body.outsideCanadaTravellingInPast14Days,
+      closeContactWithSuspectedCase: body.closeContactWithSuspectedCase,
+      unusualSevereFatigue: body.unusualSevereFatigue,
+      unusualHeadache: body.unusualHeadache,
+      significantLossOfAppetite: body.significantLossOfAppetite,
+      unusualOrUnexplainedMusclePainOrStiffness: body.unusualOrUnexplainedMusclePainOrStiffness,
+      soreThroatWithoutObviousCause: body.soreThroatWithoutObviousCause,
+    });
+
+    // Send the payload via mongoose, wait for response then return it
+    const savedHealthForm = await healthform.save();
+    response.json(savedHealthForm);
+  } else {
+    response.status(422).json({
+      error: 'Unprocessable Entity',
+    });
+  }
 });
 
 module.exports = formsRouter;

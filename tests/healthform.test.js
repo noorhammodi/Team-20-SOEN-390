a939b734f99942f2c05da81702ae03d5467592f8
Deniz Akcal <denizakcal@mail.com>
2022-03-14 06:08:54 -0400

    Revert "Merge branch 'issue-32-health-form-connection' of https://github.com/SOEN-390-Team-20/Team-20-SOEN-390 into issue-32-health-form-connection"
    This reverts commit 3d6bd5767f30399418f5a3c73d1e1d5526ad8df9, reversing
    changes made to 33067ebfb52102278ff61db85344253c8ac9b83a.

--- tests/healthform.test.js
+++ tests/healthform.test.js
@@ -9,7 +9,6 @@ const api = supertest(app);
 
 const TEST_HEALTHFORM1 = {
   feverOrChills: false,
-  temperature: 0,
   suddenLossOfSenseOfSmellAndTaste: false,
   difficultyBreathingOrShortnessOfBreath: true,
   cough: false,
@@ -26,7 +25,6 @@ const TEST_HEALTHFORM1 = {
 
 const TEST_HEALTHFORM2 = {
   feverOrChills: true,
-  temperature: 0,
   suddenLossOfSenseOfSmellAndTaste: false,
   difficultyBreathingOrShortnessOfBreath: true,
   cough: false,
@@ -92,54 +90,26 @@ const TEST_HEALTHFORM5 = {
   hin: '740WL3V05',
 };
 
-const TEST_HEALTHFORM5 = {
-  feverOrChills: true,
-  temperature: 'notANumber',
-  suddenLossOfSenseOfSmellAndTaste: false,
-  difficultyBreathingOrShortnessOfBreath: true,
-  cough: false,
-  runnyOrStuffyNose: true,
-  outsideCanadaTravellingInPast14Days: false,
-  closeContactWithSuspectedCase: true,
-  unusualSevereFatigue: false,
-  unusualHeadache: true,
-  significantLossOfAppetite: false,
-  unusualOrUnexplainedMusclePainOrStiffness: true,
-  soreThroatWithoutObviousCause: false,
-};
-
 describe('REST API request on /api/forms/healthform', () => {
   test('POST /api/forms/healthform with feverOrChills being false and temperature not being included is successful', async () => {
     await api
       .post('/api/forms/healthform')
       .send(TEST_HEALTHFORM1)
       .expect(200);
-
-    // PostCondition: Fever CheckBox is false and temp value is 0
-    expect(TEST_HEALTHFORM1.feverOrChills).toBeFalsy();
-    expect(TEST_HEALTHFORM1.temperature).toBe(0);
   });
 
-  test('POST /api/forms/healthform with feverOrChills being true and temperature not being included is successful but temperature is not taken in the database', async () => {
+  test('POST /api/forms/healthform with feverOrChills being true and temperature not being included is not successful', async () => {
     await api
       .post('/api/forms/healthform')
       .send(TEST_HEALTHFORM2)
-      .expect(200);
-
-    // PostCondition: Fever CheckBox is true and temp value is 0
-    expect(TEST_HEALTHFORM2.feverOrChills).toBeTruthy();
-    expect(TEST_HEALTHFORM2.temperature).toBe(0);
+      .expect(422);
   });
 
-  test('POST /api/forms/healthform with feverOrChills being false and temperature being included is successful but temperature are not taken in the database', async () => {
+  test('POST /api/forms/healthform with feverOrChills being false and temperature being included is not successful', async () => {
     await api
       .post('/api/forms/healthform')
       .send(TEST_HEALTHFORM3)
-      .expect(200);
-
-    // PostCondition: Fever CheckBox is false and temp value is 30
-    expect(TEST_HEALTHFORM3.feverOrChills).toBeFalsy();
-    expect(TEST_HEALTHFORM3.temperature).toBe(30);
+      .expect(422);
   });
 
   test('POST /api/forms/healthform with feverOrChills being true and temperature being included is successful', async () => {
@@ -147,20 +117,6 @@ describe('REST API request on /api/forms/healthform', () => {
       .post('/api/forms/healthform')
       .send(TEST_HEALTHFORM4)
       .expect(200);
-
-    // PostCondition: Fever CheckBox is true and temp value is 30
-    expect(TEST_HEALTHFORM4.feverOrChills).toBeTruthy();
-    expect(TEST_HEALTHFORM4.temperature).toBe(30);
-  });
-
-  test('POST /api/forms/healthform with temperature not being a number', async () => {
-    await api
-      .post('/api/forms/healthform')
-      .send(TEST_HEALTHFORM5)
-      .expect(400);
-
-    // PostCondition: expect temp value is 30 but received temp value is not a number
-    expect(TEST_HEALTHFORM5.temperature).not.toBe(30);
   });
 
   test('POST /api/forms/healthform with feverOrChills being true and temperature being included but not as a number is not successful', async () => {

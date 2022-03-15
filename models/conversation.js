2c63b7fb8708cc21f809baed505c1bdb132124c8
Jonathan Hubermann <hubjon11@gmail.com>
2022-03-15 18:26:19 -0400

    Created model to store conversations

---
+++ models/conversation.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const conversationSchema = new mongoose.Schema({
+  participants: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  ],
+  messages: [
+    {
+      sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
+      timestamp: {
+        default: Date.now(),
+        required: true,
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model('Conversation', conversationSchema);

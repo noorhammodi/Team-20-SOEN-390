00afe01322868d9e76a4efc407b5288e8d6078ff
Oliver Hassan <oliver.hassan2000@gmail.com>
2022-03-15 16:26:17 -0400

    added message sent component

---
+++ client/src/components/chat/MessageSent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MessageSent() {
+  return (
+    <div className={classes.messageRowRight}>
+      <div className={classes.messageOrange}>
+        <p className={classes.messageContent}>{message}</p>
+        <div className={classes.messageTimeStampRight}>{timestamp}</div>
+      </div>
+    </div>
+  );
+}
+
+export default MessageSent;
